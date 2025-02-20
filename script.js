e3a170340ae818dd050f4b6f958f6b72e88b0bcf
angga1837 <67214645+angga1837@users.noreply.github.com>
2025-02-20 14:06:21 +0700

    tinggal responsive degisn

--- script.js
+++ script.js
@@ -1,19 +1,23 @@
 function peluang(){
     let n = parseInt(document.getElementById("input-n").value);
     let x = parseInt(document.getElementById("input-x").value);
-    let p = parseInt(document.getElementById("input-p").value);
-    let q = parseInt(document.getElementById("input-q").value);
-    var Peluang = (factorial(n)/(factorial(x)*factorial(n-x))) * pangkat(p, x)  ;
+    let p = parseFloat(document.getElementById("input-p").value);
+    let q = parseFloat(document.getElementById("input-q").value);
+    var Peluang = parseFloat((factorial(n)/(factorial(x)*factorial(n-x))) * pangkat(p, x) * pangkat(q, n-x)) ;
     document.getElementById("hasil").innerHTML = Peluang;
     console.log(Peluang);
 
 
     function pangkat(a,b){
-        for (let i = )
+        hasilpangkat = 1
+        for (let i = 1; i <= b; i++ ){
+            hasilpangkat = hasilpangkat * a;
+        }
+        return hasilpangkat
     }
-    function factorial(x){
+    function factorial(p){
         let total = 1;
-        for (let i = x; i > 0; i-- ){
+        for (let i = p; i > 0; i-- ){
             total = total * i;
             
         };
